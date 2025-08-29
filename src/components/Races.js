4f89bbb3fb1f3a14e4d3a6e50d3f44901d6fe222
Jade Clement <jadecathclement@gmail.com>
2025-08-29 09:47:22 -0700

    Fix hardcoded localhost URL in Races component

--- src/components/Races.js
+++ src/components/Races.js
@@ -16,7 +16,7 @@ const Races = () => {
     description: ''
   });
 
-  const API_BASE_URL = 'http://localhost:5001/api';
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001/api';
 
   useEffect(() => {
     loadRaces();
